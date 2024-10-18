d3b6d1981c29da998d790d800693ab063e474e95
hardworking-toptal-dev <161673729+hardworking-toptal-dev@users.noreply.github.com>
2024-10-18 02:14:35 -0400

    feat: add sepolia network in config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,16 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL!,
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY!],
+    },
+  },
 };
 
 export default config;
